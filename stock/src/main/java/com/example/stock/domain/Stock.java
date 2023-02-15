7f13321f12305a092474b4b0583904ced1dc5524
twoosky <lhjae3@naver.com>
2023-02-15 23:29:47 +0900

    feat: Stock 엔티티 생성

---
+++ stock/src/main/java/com/example/stock/domain/Stock.java
@@ -0,0 +1,26 @@
+package com.example.stock.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productId;
+
+    private Long quantity;
+
+    public Stock(Long productId, Long quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+}
