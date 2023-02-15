efdf04d0d386188fbc4361e5a6293c85c2581907
twoosky <lhjae3@naver.com>
2023-02-16 04:09:46 +0900

    test: Lettuce 방식 멀티 스레드 테스트

---
+++ stock/src/test/java/com/example/stock/facaed/LettuceLockStockFacadeTest.java
@@ -0,0 +1,60 @@
+package com.example.stock.facaed;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.facade.LettuceLockStockFacade;
+import com.example.stock.repository.StockRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class LettuceLockStockFacadeTest {
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @Autowired
+    private LettuceLockStockFacade lettuceLockStockFacade;
+
+    @BeforeEach
+    public void before() {
+        Stock stock = new Stock(1L, 100L);
+        stockRepository.saveAndFlush(stock);
+    }
+
+    @AfterEach
+    public void after() {
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    public void 동시에_100개_요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        for(int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    lettuceLockStockFacade.decrease(1L, 1L);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+        countDownLatch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        assertThat(stock.getQuantity()).isEqualTo(0L);
+    }
+}
