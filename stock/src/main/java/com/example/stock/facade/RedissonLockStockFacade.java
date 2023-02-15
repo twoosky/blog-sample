5e99f042c00a4be11e0197a987a51993c66720bb
twoosky <lhjae3@naver.com>
2023-02-16 04:39:24 +0900

    feat: Redisson 분산락 Facade 구현

---
+++ stock/src/main/java/com/example/stock/facade/RedissonLockStockFacade.java
@@ -0,0 +1,36 @@
+package com.example.stock.facade;
+
+import com.example.stock.service.StockService;
+import lombok.RequiredArgsConstructor;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+@RequiredArgsConstructor
+public class RedissonLockStockFacade {
+
+    private final RedissonClient redissonClient;
+    private final StockService stockService;
+
+    public void decrease(Long id, Long quantity) {
+        RLock lock = redissonClient.getLock(id.toString());
+
+        try {
+            boolean available = lock.tryLock(10, 1, TimeUnit.SECONDS);
+
+            if (!available) {
+                System.out.println("lock 획득 실패");
+                return;
+            }
+
+            stockService.decrease(id, quantity);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
