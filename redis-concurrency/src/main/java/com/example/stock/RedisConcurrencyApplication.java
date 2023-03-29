5226183520badb1b877d6debca929ebd43d3af6b
twoosky <lhjae3@naver.com>
2023-03-29 16:10:21 +0900

    refactor: 패키지명 변경

--- redis-concurrency/src/main/java/com/example/stock/RedisConcurrencyApplication.java
+++ redis-concurrency/src/main/java/com/example/stock/RedisConcurrencyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StockApplication {
+public class RedisConcurrencyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StockApplication.class, args);
+		SpringApplication.run(RedisConcurrencyApplication.class, args);
 	}
 
 }
--- redis-concurrency/src/test/java/com/example/stock/RedisConcurrencyApplicationTests.java
+++ redis-concurrency/src/test/java/com/example/stock/RedisConcurrencyApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class StockApplicationTests {
+class RedisConcurrencyApplicationTests {
 
 	@Test
 	void contextLoads() {
